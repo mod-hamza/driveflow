98ed5506cbc5082563514974cf5265a50c910776
thg-001 <nimeshuggah@gmail.com>
2026-08-13 11:07:05 +0400

    Add the first Drive Flow fleet screen
    Wire up the Cars menu, list and form views, and access rules so the app has a usable starting point.

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,10 @@
 {
-    'name':'Drive Flow',
-    'depends':['account'],
-    'application':True,
-    'data':[
-        # File changes come here
-    ]
+    "name": "Drive Flow",
+    "depends": ["account"],
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/driveflow_car_views.xml",
+        "views/driveflow_menus.xml",
+    ],
 }
